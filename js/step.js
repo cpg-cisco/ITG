fe77f31751faa62075a579e0411b45fcd15193d5
Derek Zhang <zhiyzhan@cisco.com>
2014-08-05 11:16:22 -0700

    Move the nav to js file to dynamically generate the nav

---
+++ js/step.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('#navigation_bar').append('<li class="active"><a href="./index.html">Home</a></li>');
+	$('#navigation_bar').append('<li class=""><a href="./timeline.html">Timeline</a></li>');
+	$('#navigation_bar').append('<li class=""><a href="./about.html">About</a></li>');
+	$('#navigation_bar_right').append('<li class="active"><a href="./index.html">Fixed top</a></li>');
+});
